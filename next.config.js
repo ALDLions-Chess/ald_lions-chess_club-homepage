b888fb8fe7ab30bac1a16862ac3eb945f3673a6e
Alv-P <alvinpaulp08@gmail.com>
2025-10-22 16:11:38 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'ald-chess_club-homepage'; // Change if your repo name is different
+
 const nextConfig = {
   output: 'export',
-  basePath: '/ald_lions-chess_club-homepage',
-  assetPrefix: '/ald_lions-chess_club-homepage/',
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
