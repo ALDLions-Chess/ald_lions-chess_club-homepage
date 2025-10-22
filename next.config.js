6ecf2ba5e3c4d04df28a2bf9d7611752d26ae9e3
Alv-P <alvinpaulp08@gmail.com>
2025-10-22 13:51:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // If your repo is not at the root, set basePath and assetPrefix:
-  // basePath: '/ald-chess_club-homepage',
-  // assetPrefix: '/ald-chess_club-homepage/',
+  basePath: '/ald_lions-chess_club-homepage',
+  assetPrefix: '/ald_lions-chess_club-homepage/',
 };
 
 module.exports = nextConfig;
